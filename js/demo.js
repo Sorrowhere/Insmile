d46ebd6f5ffa7d54b22240067da980227547068b
sorrowhere <sorrowhere@gmail.com>
2018-06-28 14:16:20 +0800

    Aboutus page complete

--- js/demo.js
+++ js/demo.js
@@ -7,4 +7,11 @@ $(function(){
     });
 
 
+    var $tabMenu = $('.sidebar a');
+    $tabMenu.click(function(){
+       $(this).addClass('on').siblings().removeClass('on');
+        var tabIndex = $tabMenu.index(this);
+        $('.tab-con > section').eq(tabIndex).addClass('active').siblings().removeClass('active');
+    });
+
 })
